3f5553e610a7d4027abfb0b338f0007302332c6f
Oladayo Fagbemi <oofagbemi@outlook.com>
2019-08-20 08:00:06 +0200

    added new navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar">
+{{-- <nav class="navbar">
     <div class="navbar__logo">
         <h2>
             <a href="{{ url('/') }}">
@@ -12,11 +12,24 @@
         <li class="navbar__item"><a href="/">Home</a></li>
         <li class="navbar__item"><a href="#">About Me</a></li>
         <li class="navbar__item"><a href="#">Profile</a></li>
-        <li class="username">{{ Auth::user()->name }}</li>
-
+        <li class="username">
+        <span> {{ Auth::user()->name }}</span>
+        <a href="#" class="triggerdropdown">
+        <i class="material-icons">arrow_drop_down</i>
+        </a>
+        <ul class="dropdown">
         <li><a href="{{ route('logout') }}" class="" onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="navbar__actions-hidden">
+                document.getElementById('logout-form').submit();">{{ __('Logout') }}
+            </a>
+        </li>
+        </ul>
+       </li>
+
+       
+
+        {{-- <li><a href="{{ route('logout') }}" class="" onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li> --}}
+       {{-- <form id="logout-form" action="{{ route('logout') }}" method="POST" class="navbar__actions-hidden">
             {{ csrf_field() }}
         </form>
         @else
@@ -26,4 +39,48 @@
         @endif
         @endauth
     </ul>
+</nav> --}}
+
+
+
+
+<nav class="navbar">
+    <div class="navbar__logo">
+        <h2>
+            <a href="{{ url('/') }}">
+             {{ config('app.name', 'DevConnect') }}
+            
+            </a>
+        </h2>
+    </div>
+
+    <div class="navbar__links">
+        @auth
+        <a class="navbar__item" href="/">Home</a>
+        <a class="navbar__item" href="#">About Me</a>
+        <a class="navbar__item" href="#">Profile</a>
+        <div class="username">
+        <p> {{ Auth::user()->name }}</p>
+        <i class="material-icons">arrow_drop_down</i>
+       
+        <div class="dropdown">
+        <div class="logout">
+        <a href="{{ route('logout') }}"  onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">{{ __('Logout') }}
+        </a>
+         <i class="material-icons" style="color:orangered">exit_to_app</i> 
+        </div>
+        
+        </div>
+       </div>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="navbar__actions-hidden">
+            {{ csrf_field() }}
+        </form>
+        @else
+        <li class="navbar__item"><a class="" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+        @if (Route::has('register'))
+        <li class="navbar__item"><a class="" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+        @endif
+        @endauth
+    </div>
 </nav>
